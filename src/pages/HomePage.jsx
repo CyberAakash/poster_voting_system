0a96661526d4279776d97afbb5b84aa3c1265fa6
CyberAakash <aakashvnth@gmail.com>
2023-07-20 23:03:01 +0530

    again came to right point

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
-import Banner from "./components/Banner";
-import Carousel from "./components/Carousel";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Banner from "../components/Banner";
+import Carousel from "../components/Carousel";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 function HomePage() {
   return (
--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer";
-import Form from "./components/Form";
-import Navbar from "./components/Navbar";
+import Form from "../components/Form";
+import Navbar from "../components/Navbar";
 
 
 function VotePage() {
